dea42d7950b5d489e7af89b1c876481eed522ecc
saber <saber@work.app>
2025-07-02 15:19:50 +0800

    分包支持

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -55,6 +55,15 @@ android {
             signingConfig = signingConfigs.getByName("release")
         }
     }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a")
+            isUniversalApk = false
+        }
+    }
 }
 
 flutter {
